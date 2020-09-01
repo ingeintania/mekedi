ac1eaf0fd4857d8d1c8928b0cc13a26590c617c5
prk <prk@bn71109.local>
2020-09-01 10:38:01 +0700

    Initial Commit

---
+++ mekedi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mekedi
+//
+//  Created by prk on 01/09/20.
+//  Copyright © 2020 prk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
